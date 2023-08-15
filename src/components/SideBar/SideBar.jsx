dad976fa31023de7ac9ed3b04ebfeeb0c12f6d59
Mayank <97655679+mayankAd@users.noreply.github.com>
2023-08-15 20:05:01 +0530

    Initial commit

---
+++ src/components/SideBar/SideBar.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+const SideBar = () => {
+  return (
+    <>
+    <div className="logo">
+    <img src="" alt="" />
+    <div className="website_title">
+        <a href='#'>Muscle Showroom</a>
+    </div>
+    </div>
+    <div className="types">
+        <a href="#">ACCESSORIES</a>
+        <a href="#">CLOTHING</a>
+        <a href="#">SUPPLEMENTS</a>
+        <a href="#">EQUIPMENTS</a>
+        <a href="#">YOGA</a>
+        <a href="#">DIET</a>
+        <a href="#">CLOTHING</a>
+        <a href="#">SALE</a>
+        <a href="#">ONLINE TRAINING</a>
+        <a href="#">BRANDS</a>
+        <a href="#">VITAMINS</a>
+        <a href="#">ESSENTIALS</a>
+    </div>
+    <div className="footer_text">
+    <a href="#">About Us</a>
+    <a href="#">Visit Us</a>
+    <a href="#">Blog</a>
+    <a href="#">Shipping & Privacy</a>
+    </div>
+    <div className="footer_logos">
+    <a href="#">f</a>
+    <a href="#">t</a>
+    <a href="#">i</a>
+    <a href="#">y</a>
+    <a href="#">s</a>
+    </div>
+    </>
+  )
+}
+
+export default SideBar
